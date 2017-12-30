fd2f1417b1209ef0ba093b681726bd432f697bc0
Christopher Dorst <chris@dorst.email>
2017-12-30 15:33:03 -0600

    Add StringGenerator

---
+++ Common.Functions.GetRandomAlphaNumericString/StringGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Common.Functions.GetRandomAlphaNumericString
+{
+    public static class StringGenerator
+    {
+        private const string AlphaNumericChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        private const byte AlphaNumericCharsLength = 62; // == AlphaNumericChars.Length
+
+        public static string Random(byte length)
+            => new string(GetRandomChars(length));
+
+        private static char[] GetRandomChars(byte length)
+        {
+            var rng = new Random();
+            var chars = new char[length];
+            for (var i = byte.MinValue; i < length; i++)
+                chars[i] = NextChar(rng);
+            return chars;
+        }
+
+        private static char NextChar(Random rng)
+            => AlphaNumericChars[rng.Next(AlphaNumericCharsLength)];
+    }
+}
